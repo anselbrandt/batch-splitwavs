dac056fa4bd8140210dd527ea2576cb0e664a9f3
anselbrandt <ansel@anselbrandt.com>
2024-12-06 16:11:12 -0500

    Update to handle clean audio

--- split.py
+++ split.py
@@ -1,16 +1,33 @@
 import os
+import time
+import logging
 from utils import getTranscriptFiles, srt_to_transcript, split_waveform_by_timestamps
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[logging.FileHandler("logs.txt"), stream_handler],
+)
 
 ROOT = os.getcwd()
 
-filesDir = os.path.join(ROOT, "output")
-wavsDir = os.path.join(ROOT, "audio")
-outputDir = os.path.join(ROOT, "splitwavs")
+isCleanAudio = True if os.path.isdir("clean_audio") else False
+
+filesDir = os.path.join(ROOT, "clean_output" if isCleanAudio else "output")
+wavsDir = os.path.join(ROOT, "clean_audio" if isCleanAudio else "audio")
+outputDir = os.path.join(ROOT, "clean_splitwavs" if isCleanAudio else "splitwavs")
 os.makedirs(outputDir, exist_ok=True)
 files = getTranscriptFiles(filesDir)
 
 for filepath, showname, filename in files:
+    start_time = time.time()
     episode = filename.split("_-_")[0] if "_-_" in filename else filename.split(".")[0]
     transcript = srt_to_transcript(filepath)
     input_file = os.path.join(wavsDir, showname, filename.replace(".srt", ".wav"))
     split_waveform_by_timestamps(input_file, outputDir, transcript, showname, episode)
+    execution_time = time.time() - start_time
+    logging.info(f"{showname}/{filename}|{execution_time}")
