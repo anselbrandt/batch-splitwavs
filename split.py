e8c2c3253be7c9fc4ca049db5bb5b39460a29d9c
anselbrandt <ansel@anselbrandt.com>
2024-12-05 20:55:17 -0500

    Update episode var

--- split.py
+++ split.py
@@ -10,7 +10,7 @@ os.makedirs(outputDir, exist_ok=True)
 files = getTranscriptFiles(filesDir)
 
 for filepath, showname, filename in files:
-    episode = filename.split("_-_")[0]
+    episode = filename.split("_-_")[0] if "_-_" in filename else filename.split(".")[0]
     transcript = srt_to_transcript(filepath)
     input_file = os.path.join(wavsDir, showname, filename.replace(".srt", ".wav"))
     split_waveform_by_timestamps(input_file, outputDir, transcript, showname, episode)
